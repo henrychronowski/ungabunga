a3a9687feaf62caaca31bf806b4680b4fd373316
henry.chronowski <henry.chronowski@mymail.champlain.edu>
2021-10-03 18:09:16 -0400

    It spawns a sword now, don't ask questions

---
+++ UngaBunga/Source/UngaBunga/WeaponBase.h
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "WeaponBase.generated.h"
+
+UCLASS()
+class UNGABUNGA_API AWeaponBase : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AWeaponBase();
+
+	UFUNCTION(BlueprintCallable)
+	void SetCanHit(bool param) { canHit = param; };
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UPROPERTY(BlueprintReadWrite)
+	bool canHit;
+
+};
